d5be9b3bd66594b9ca962d0964ae4069e99e77ef
Andrea Munari <andrea.munari@iticopernico.it>
2025-09-04 15:19:40 +0000

    Sep 4, 2025, 5:19 PM

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ C_Max = 1500                       # capitale massimo
 
 P_max = 50000                      # numero massimo di partite giocate
 
+#Simulare il capitale del giocatore nel tempo usando un approccio Monte Carlo: ad ogni passo
+#andrà simulato il risultato della singola partita e aggiornato il capitale. Si imposti un numero
+#massimo di partite (per evitare cicli infiniti) di almeno 50.000 partite.
+
+C = C_0
+n_partite = 0
+while C > 0 and C < C_Max and n_partite < P_max:
+    if random.random() < p:
+        C += δ
+    else:
+        C -= δ
+    n_partite += 1
+    if n_partite % 1000 == 0:
+        print(f"Partite giocate: {n_partite}, Capitale attuale: {C}")
+print(f"Partite giocate: {n_partite}, Capitale finale: {C}")
+#Si stampi a video il capitale del giocatore ogni 1000 partite giocate.
